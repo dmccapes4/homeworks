0a3d1d61fafe662e17841c0bd7c10cfa8627d76d
Dylan <dmccapes@mac.com>
2017-12-08 06:45:17 -0800

    Complete w1d5 hw

---
+++ W1D5/map.rb
@@ -0,0 +1,45 @@
+class Array
+  def second
+    self[1]
+  end
+end
+
+
+class Map
+  attr_reader :map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+     @map.each_with_index do |kv_pair, idx|
+       if kv_pair.first == key
+         @map[idx] = [key, value]
+         return @map[idx]
+       end
+     end
+     @map << [key, value]
+  end
+
+  def lookup(key)
+    @map.each do |kv_pair|
+      return kv_pair.second if kv_pair.first == key
+    end
+    false
+  end
+
+  def remove(key)
+    @map.each_with_index do |kv_pair, idx|
+      if kv_pair.first == key
+        @map.delete_at(idx)
+        return kv_pair
+      end
+    end
+    false
+  end
+
+  def show
+    @map
+  end
+end
